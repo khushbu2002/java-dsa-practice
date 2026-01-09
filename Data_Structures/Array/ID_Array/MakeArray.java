c215a7531fa0e46097fe724d4d852c236f0c317e
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2026-01-09 19:31:13 +0530

    Add basic array input and output program in Java

---
+++ Data_Structures/Array/ID_Array/MakeArray.java
@@ -0,0 +1,20 @@
+package Data_Structures.Array.ID_Array;
+
+import java.util.Scanner;
+
+public class MakeArray {
+    public static void main(String arg[]) {
+        int size = 5;
+        int[] arr = new int[size];
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<size; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
