96a0e7a441275ac7c560d1969ee244978c99e036
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2026-01-25 00:29:00 +0530

    make String using String and StringBuilder

--- Data_Structures/String/MakeString.java
+++ Data_Structures/String/MakeString.java
@@ -1,7 +1,7 @@
 package String;
 
 public class MakeString {
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
 
         String s1 = "Hello";
         System.out.println("String: " + s1);
