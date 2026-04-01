2662fbf0025ab4d851486a142f3ab1fce2c3cdae
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2026-04-01 10:29:23 +0530

    Check wheather a String is a palindrome or not.

---
+++ Data_Structures/String/CheckPalindromeString.java
@@ -0,0 +1,25 @@
+package String;
+
+public class CheckPalindromeString {
+    static boolean  checkPalindromeString(String input) {
+        boolean result = true;
+        int length = input.length();
+
+        for (int i = 0; i < length/2; i++) {
+            if (input.charAt(i) != input.charAt(length - i - 1)) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String arg[]) {
+        String s = "abrakarba";
+
+        System.out.print(checkPalindromeString(s));
+
+    }
+
+}
