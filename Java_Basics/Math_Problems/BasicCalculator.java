c3e45ea45ceca912ec52ec512d076afdc4ca3071
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2026-04-03 15:11:20 +0530

    Basic Calculator that takes 2 operands and perform the operation to give desired result.

---
+++ Java_Basics/Math_Problems/BasicCalculator.java
@@ -0,0 +1,41 @@
+import java.lang.classfile.instruction.SwitchCase;
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+
+    public static void main(String arg[]) {
+       Scanner sc = new Scanner(System.in);
+       int operend1;
+        int operend2;
+
+       System.out.println("Please enter 2 numbers to perform action: ");
+
+       operend1 = sc.nextInt();
+       operend2 = sc.nextInt();
+
+        System.out.println("Please choose the operation to be performed: ");
+        String operator = sc.next();
+
+        switch(operator) {
+            case "+":
+                System.out.println("your answer is = " + (operend1 + operend2));
+                break;
+            case "-":
+                System.out.println("your answer is = " + (operend1 - operend2));
+                break;
+            case "/":
+                System.out.println("your answer is =" + (operend1 / operend2));
+                break;
+            case "*":
+                System.out.println("your answer is =" + (operend1 * operend2));
+                break;
+            case "%":
+                System.out.println("your answer is =" + (operend1 % operend2));
+                break;
+            default: System.out.println("nothing choosen");
+        }
+
+    }
+
+}
