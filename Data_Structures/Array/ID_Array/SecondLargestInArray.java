9e7630df1db3f315baaa1ec0037e224a918116a0
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2026-04-06 15:01:04 +0530

    Find second largest number in an array.

---
+++ Data_Structures/Array/ID_Array/SecondLargestInArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class SecondLargestInArray {
+
+    public static int secondLargestElementInArray (int arr[]) {
+
+        if(arr.length == 0 ) {
+            System.out.println("there is no element");
+            return 0;
+        }
+        if(arr.length == 1 ) {
+            System.out.println("there is only one element and that is: ");
+            return arr[0];
+        }
+
+        Arrays.sort(arr);
+
+        return arr[arr.length-2];
+
+        // i will write another function with another logic
+
+    }
+
+    public static void main(String arg[]) {
+        int[] arr1 = {};
+        int[] arr2 = {2};
+        int[] arr3 = {5,9,2,4,8,1,-1};
+
+        System.out.println(secondLargestElementInArray(arr1));
+        System.out.println(secondLargestElementInArray(arr2));
+        System.out.println(secondLargestElementInArray(arr3));
+    }
+
+}
