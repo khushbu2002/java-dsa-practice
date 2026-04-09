38b88501ef3525eb4f087eba3452a9bc91f91e66
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2026-04-09 17:55:00 +0530

    Compare 2 strings.

--- Data_Structures/String/CheckStringsEqual.java
+++ Data_Structures/String/CheckStringsEqual.java
@@ -4,7 +4,6 @@ public class CheckStringsEqual {
     public static void main(String arg[]) {
         String s1 = "Hello";
         String s2 = "Hello";
-
         boolean b = s1.equals(s2);
 
         System.out.println(b); //simple and easy
