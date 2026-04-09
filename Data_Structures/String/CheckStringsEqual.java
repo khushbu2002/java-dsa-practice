04e1430d44561cfbc0de599e808a34264af5e220
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2026-04-09 14:07:06 +0530

    Compare 2 strings.

---
+++ Data_Structures/String/CheckStringsEqual.java
@@ -0,0 +1,12 @@
+package String;
+
+public class CheckStringsEqual {
+    public static void main(String arg[]) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        boolean b = s1.equals(s2);
+
+        System.out.println(b);
+    }
+}
